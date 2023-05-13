2aedd3afb430f091b44b4f2ccb98a64b50eeb0dc
Kendra Bryant <kbryant@efilecabinet.com>
2023-05-13 17:27:17 -0600

    Pulled logging statement

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -2,7 +2,6 @@ const baseURL = import.meta.env.VITE_SERVER_URL;
 
 async function convertToJson(res) {
   let jsonResponse = await res.json();
-  console.log({ jsonResponse });
   if (jsonResponse.Result || jsonResponse.message) {
     return jsonResponse;
   } else {
